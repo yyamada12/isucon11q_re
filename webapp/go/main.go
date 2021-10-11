d9c03aecd7ea35906b3cdba1481370e767ca0310
yyamada12 <12yacropolisy@gmail.com>
2021-10-11 13:03:08 +0900

    app log off [19860]

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -214,10 +214,9 @@ func main() {
 	}()
 
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	e.Debug = false
+	e.Logger.SetLevel(log.OFF)
 
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.POST("/initialize", postInitialize)
